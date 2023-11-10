917a9e009d08a21004fd950e3d0199d52423c58d
Hans Schwaiger <hschwaiger@usgs.gov>
2023-11-09 23:42:29 -0900

    Modified setup scripts to test for WINDROOT

--- examples/Mazama_DepUmb/setup.sh
+++ examples/Mazama_DepUmb/setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+if [ -z ${WINDROOT} ];then
+ # Standard Linux location
+ WINDROOT="/data/WindFiles"
+ # Mac
+ #WINDROOT="/opt/data/WindFiles"
+fi
 ln -s ../../bin/Ash3d Ash3d
-ln -s /data/WindFiles/NCEP .
+ln -s ${WINDROOT}/NCEP .
