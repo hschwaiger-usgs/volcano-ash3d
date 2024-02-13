bdf15538af2c151fbf0782084086f4d957fc5134
Hans F. Schwaiger <hschwaiger@usgs.gov>
2024-02-12 17:28:06 -0900

    Updated get_version.sh to workk on both linux and Mac.

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,3 +1,2 @@
-echo -e "      character(len=40),parameter,public :: Ash3d_GitComID ='\c" > Ash3d_version.h
-git log -n 1 | grep commit | cut -f 2 -d' ' | tr -d $'\n' >> Ash3d_version.h
-echo -e "'\c" >> Ash3d_version.h
+gID=`git log -n 1 | grep commit | cut -f 2 -d' ' | tr -d $'\n'` 
+echo "      character(len=40),parameter,public :: Ash3d_GitComID ='${gID}'" > Ash3d_version.h 
