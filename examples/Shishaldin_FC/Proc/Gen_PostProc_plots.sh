d9aa4e1b5520a46e04051ce4f948d22fb5bf856d
Hans <hschwaiger@usgs.gov>
2025-07-24 17:34:30 -0800

    Added a bit to the Shishaldin postprocesing.

--- examples/Shishaldin_FC/Proc/Gen_PostProc_plots.sh
+++ examples/Shishaldin_FC/Proc/Gen_PostProc_plots.sh
@@ -4,14 +4,30 @@
 rm depothik.* Ash3d_CloudLoad_t0*png Ash3d_Deposit____final.png plplt_000*png
 
 ln -s ../../../bin/Ash3d_PostProc .
-ln -s ../3d_tephra_fall.nc .
-infile=3d_tephra_fall.nc
+ln -s ../3d_tephra_fallLL.nc .
+infile1=3d_tephra_fallLL.nc
+ln -s ../3d_tephra_fallPJ.nc .
+infile2=3d_tephra_fallPJ.nc
 
 # Generate vertical profile plots using plplot (change to ASH3DPLOT=3 for gnuplot)
-ASH3DPLOT=2 ./Ash3d_PostProc ${infile} 16 3
+ASH3DPLOT=2 ./Ash3d_PostProc ${infile1} 16 3
+mv gnupl_0001.png gnupl_0001_LL.png
+mv gnupl_0002.png gnupl_0002_LL.png
+mv gnupl_0003.png gnupl_0003_LL.png
 
 # Generate shapefile output of deposit thickness in mm
-ASH3DPLOT=3 ./Ash3d_PostProc ${infile} 5 3
+ASH3DPLOT=4 ./Ash3d_PostProc ${infile1} 5 3
+mv Ash3d_Deposit____final.png Ash3d_Deposit____final_LL.png
+
+# Generate vertical profile plots using plplot (change to ASH3DPLOT=3 for gnuplot)
+ASH3DPLOT=2 ./Ash3d_PostProc ${infile1} 16 3
+mv gnupl_0001.png gnupl_0001_PJ.png
+mv gnupl_0002.png gnupl_0002_PJ.png
+mv gnupl_0003.png gnupl_0003_PJ.png
+
+# Generate shapefile output of deposit thickness in mm
+ASH3DPLOT=4 ./Ash3d_PostProc ${infile2} 5 3
+mv Ash3d_Deposit____final.png Ash3d_Deposit____final_PJ.png
 
 # Generate map of deposit with custom contour levels (GMT) using a control file
 #./Ash3d_PostProc pp.ctr
