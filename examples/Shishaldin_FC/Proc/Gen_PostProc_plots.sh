3d9ed8f2511d2046c9948a0d90171109c12b7489
Hans <hschwaiger@usgs.gov>
2025-08-11 17:27:54 -0800

    Added some more input files for testing of Shishaldin FC/RN runs with VarDiff

--- examples/Shishaldin_FC/Proc/Gen_PostProc_plots.sh
+++ examples/Shishaldin_FC/Proc/Gen_PostProc_plots.sh
@@ -1,42 +1,23 @@
 #!/bin/bash
 
-# clean up
-rm depothik.* Ash3d_CloudLoad_t0*png Ash3d_Deposit____final.png plplt_000*png
+# Set RunClass to one of:
+# PJ_091 LL_091 PJ_242 LL_242 
+# LL_gfs LL_ecmwf
+# LL_NARR LL_ERA5 LL_NASA
+RunClass="LL_091"
+mkdir -p ${RunClass}
 
 ln -s ../../../bin/Ash3d_PostProc .
-ln -s ../3d_tephra_fall_LL_091.nc .
-infile1=3d_tephra_fall_LL_091.nc
-ln -s ../3d_tephra_fall_PJ_091.nc .
-infile2=3d_tephra_fall_PJ_091.nc
+ln -s ../3d_tephra_fall_${RunClass}.nc intemp.nc
+infile=intemp.nc
 
 # Generate vertical profile plots using plplot (change to ASH3DPLOT=3 for gnuplot)
-ASH3DPLOT=2 ./Ash3d_PostProc ${infile1} 16 3
-mv gnupl_0001.png gnupl_0001_LL.png
-mv gnupl_0002.png gnupl_0002_LL.png
-mv gnupl_0003.png gnupl_0003_LL.png
+ASH3DPLOT=2 ./Ash3d_PostProc ${infile} 16 3
+mv gnupl_0001.png ${RunClass}/gnupl_0001_LL.png
+mv gnupl_0002.png ${RunClass}/gnupl_0002_LL.png
+mv gnupl_0003.png ${RunClass}/gnupl_0003_LL.png
 
 # Generate shapefile output of deposit thickness in mm
-ASH3DPLOT=4 ./Ash3d_PostProc ${infile1} 5 3
-mv Ash3d_Deposit____final.png Ash3d_Deposit____final_LL.png
-
-# Generate vertical profile plots using plplot (change to ASH3DPLOT=3 for gnuplot)
-ASH3DPLOT=2 ./Ash3d_PostProc ${infile2} 16 3
-mv gnupl_0001.png gnupl_0001_PJ.png
-mv gnupl_0002.png gnupl_0002_PJ.png
-mv gnupl_0003.png gnupl_0003_PJ.png
-
-# Generate shapefile output of deposit thickness in mm
-ASH3DPLOT=4 ./Ash3d_PostProc ${infile2} 5 3
-mv Ash3d_Deposit____final.png Ash3d_Deposit____final_PJ.png
-
-# Generate map of deposit with custom contour levels (GMT) using a control file
-#./Ash3d_PostProc pp.ctr
-
-# Generate animation of cloud load using gnuplot
-#tmax=`ncdump     -h ${infile} | grep "t = UNLIMITED" | grep -v pt | cut -c22-23` # maximum time dimension
-#for t in `seq 0 $((tmax-1))`;
-#do
-#  ASH3DPLOT=3 ./Ash3d_PostProc ${infile} 12 3 $t
-#done
-#convert -delay 25 -loop 0 `ls -1tr Ash3d_CloudLoad_t*.png` CloudLoad_animation.gif
+ASH3DPLOT=4 ./Ash3d_PostProc ${infile} 5 3
+mv Ash3d_Deposit____final.png ${RunClass}/Ash3d_Deposit____final_LL.png
 
---
+++ examples/Shishaldin_FC/Proc/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# clean up
+rm -f depothik.*
+rm -f Ash3d_CloudLoad_t0*png
+rm -f Ash3d_Deposit____final.png
+rm -f plplt_000*png 3d*nc
+
